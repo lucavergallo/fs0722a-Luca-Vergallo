90d0d3709871ae7be79c4db531bfee0c93ad56f0
lucavergallo <l.vergallo@hotmail.it>
2022-11-08 15:08:11 +0100

    esercitazione m1w3d2

---
+++ m1/w3/d1/D6.js
@@ -0,0 +1,107 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
+*/
+function area(l1, l2) { 
+    return l1 * l2;
+}
+
+
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+function crazySum(n, m) {
+    if (n == m) { return 6 * n; }
+    else { return n + m; }
+}
+
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+function crazyDiff(n) { 
+    if (n > 19) { return 3 * (n - 19); }
+    else { return n - 19; }
+}
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+function boundary(n) {
+    switch (true) {
+        case n == 400: return true; break;
+        case n >= 20 || n <= 100: return true; break;
+        default: return false;
+    }
+}
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+    
+    //function per comparare il carattere i-esimo di due stringhe
+
+    let epi = "EPICODE";
+    function epify(str) {
+        let textStr = str.substring(0, 7); //prende la sottostringa dei primi 6 caratteri
+        if (textStr != epi) { return str = epi + str; }
+        else return str;
+    }
+    
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+function check3and7(n) { 
+    if (n % 3 == 0 || n % 7 == 0) { console.log("divisibile"); }
+    else { console.log("non divisibile"); }
+}
+
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+function reverseString(str) {
+    let strReversed=[]; //serve a contenere i caratteri di str in ordine inverso
+    let rev = ""; //conterrà la stringa invertita
+    for (let i = 0; i < str.length; i++) { 
+        strReversed[i] = str[str.length - 1 - i];
+        rev += strReversed[i];    
+    }
+    return rev;
+}
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
---
+++ m1/w3/d2/D7.js
@@ -0,0 +1,313 @@
+/* ESERCIZIO 1
+  Scrivi una funzione per concatenare due stringhe ricevute come parametri, selezionando solamente i primi 2 caratteri della
+  prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
+*/
+
+function stringhe(str1, str2) {
+    let s = str1.slice(0, 2);
+    let t = str2.slice(str2.length - 3);
+    return s.concat(t).toUpperCase();
+  }
+  
+  /* ESERCIZIO 2
+    Scrivi una funzione per creare un array di soli valori DISPARI da 1 a 100.
+   */
+  let arr = [];
+  for (let i = 0; i < 100; i++) {
+    if (i % 2 != 0) {
+      arr.push(i);
+    }
+  }
+  console.log(arr);
+  /* ESERCIZIO 3
+    Scrivi una funzione per creare un array di soli valori multipli di 5, da 1 a 100.
+   */
+  
+  const skipFive = function () {
+    let array = [];
+    for (let i = 1; i < 100; i++) {
+      if (i % 5 === 0) {
+        array.push(i);
+      }
+    }
+    console.log(array);
+  };
+  
+  /*arr = []; //resetto arr
+  for (let i = 1; i < 100; i++) {
+    if (i % 5 == 0) {
+      arr.push(i);
+    }
+  }
+  console.log(arr);*/
+  /* ESERCIZIO 4
+    Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 100 (incluso).
+   */
+  let randArr = [];
+  for (i = 0; i < 10; i++) {
+    randArr.push(Math.round(Math.random() * 100));
+  }
+  
+  /* ESERCIZIO 5
+    Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici.
+   */
+  
+  //scrivo una funzione che ritorna un array con i valori pari dell'argomento
+  
+  arr = [];
+  for (let i = 0; i < 100; i++) {
+    arr[i] = i;
+  }
+  
+  function ritornaArrayConIPari(arr) {
+    return arr.filter((e) => {
+      return e % 2 == 0;
+    });
+  }
+  
+  /* ESERCIZIO 6
+    Scrivi una funzione per sommare a catena i numeri contenuti in un array.
+   */
+  
+  const sommaArray = function (arr) {
+    let s = 0;
+    arr.forEach((element) => {
+      s += element;
+    });
+    return s;
+  };
+  
+  /* ESERCIZIO 7
+    Scrivi una funzione per incrementare di 1 tutti i valori numerici in un array.
+  */
+  
+  const addOne = function (arr) {
+    return arr.map((e) => {
+      return (e = e + 1);
+    });
+  };
+  
+  /* ESERCIZIO 8
+    Sostituisci ogni stringa contenuta in un array con un numero rappresentante la sua lunghezza.
+    es.: ["EPICODE", "is", "great"] => [7, 2, 5]
+  */
+  
+  let animali = ["Adinolfi", "Berlu", "Salvini"];
+  function strToNumArray(array) {
+    let s = [];
+    for (let u of array) {
+      s.push(u.length);
+    }
+    return s;
+  }
+
+  /* ESERCIZIO 9 (EXTRA)
+  Scrivi una funzione per eliminare solo i valori PARI da un array.
+*/
+
+const deleteEvenFromArray = function (arr) {
+    return arr.filter((e) => {
+      return (e + 1) % 2 == 0;
+    });
+  };
+  
+  /* ESERCIZIO 10 (EXTRA)
+    Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 10 (incluso), SENZA AMMETTERE DUPLICATI.
+   */
+  
+  //riceve un array non vuoto di numeri
+  //e ritorna true se le entrate sono tutte diverse, false altrimenti
+  function verificaTuttiDiversi(arr) {
+    //caso array non vuoto
+    if (arr.length > 0) {
+      for (let u of arr) {
+        if (arr.indexOf(u) < arr.lastIndexOf(u)) {
+          //se ci sono due copie di u in arr
+          return false;
+        }
+      }
+      return true;
+    } 
+    }
+  
+  
+  function tenRandomWithoutClones() {
+    arr = [];
+    i = 0;
+    while(i<10) {
+      rnd = Math.round(Math.random() * 10); //genero un numero casuale
+      let arr1 = arr.concat([rnd]); //arr1 è arr concatenato al nuovo rnd
+      
+      if (verificaTuttiDiversi(arr1)) { //se rnd è diverso da tutti gli elementi precedenti
+        arr.push(rnd); //lo aggiungo ad arr
+        i = i + 1;
+      }
+    }
+    return arr;
+  }
+  
+  /* ESERCIZIO 11 (EXTRA)
+    Scrivi un algoritmo in grado di invertire un array.
+    es: [1, 3, 5] ==> [5, 3, 1]
+  */
+  const invertiArray = function (array) {
+    let arrayReversed = [];
+    let l = array.length;
+    for (i = 0; i < l; i++) { 
+      arrayReversed[i] = array[l-1-i];
+    }
+    return arrayReversed;
+  }
+  
+  
+  /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
+  const movies = [
+    {
+      Title: "The Lord of the Rings: The Fellowship of the Ring",
+      Year: "2001",
+      imdbID: "tt0120737",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings: The Return of the King",
+      Year: "2003",
+      imdbID: "tt0167260",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings: The Two Towers",
+      Year: "2002",
+      imdbID: "tt0167261",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of War",
+      Year: "2005",
+      imdbID: "tt0399295",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lords of Dogtown",
+      Year: "2005",
+      imdbID: "tt0355702",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings",
+      Year: "1978",
+      imdbID: "tt0077869",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of the Flies",
+      Year: "1990",
+      imdbID: "tt0100054",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lords of Salem",
+      Year: "2012",
+      imdbID: "tt1731697",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+      Year: "1984",
+      imdbID: "tt0087365",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of the Flies",
+      Year: "1963",
+      imdbID: "tt0057261",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Avengers",
+      Year: "2012",
+      imdbID: "tt0848228",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Infinity War",
+      Year: "2018",
+      imdbID: "tt4154756",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Age of Ultron",
+      Year: "2015",
+      imdbID: "tt2395427",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Endgame",
+      Year: "2019",
+      imdbID: "tt4154796",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+    },
+  ];
+  
+  /* ESERCIZIO 12
+    Scrivi una funzione per trovare il film più vecchio nell'array fornito.
+  */
+  
+ 
+  
+  /* ESERCIZIO 13
+    Scrivi una funzione per ottenere il numero di film contenuti nell'array fornito.
+  */
+  
+  
+  
+  /* ESERCIZIO 14
+    Scrivi una funzione per creare un array con solamente i titoli dei film contenuti nell'array fornito.
+  */
+  
+ 
+  /* ESERCIZIO 15
+    Scrivi una funzione per ottenere dall'array fornito solamente i film usciti nel millennio corrente.
+  */
+  
+ 
+  /* ESERCIZIO 16
+    Scrivi una funzione per ottenere dall'array fornito uno specifico film (la funzione riceve un imdbID come parametro).
+  */
+  
+  /* ESERCIZIO 17
+    Scrivi una funzione per calcolare la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array fornito.
+  */
+ 
+  
+  /* ESERCIZIO 18 (EXTRA)
+    Scrivi una funzione per recuperare tutti i film dall'array fornito che contengono una parola fornita.
+  */
+  
